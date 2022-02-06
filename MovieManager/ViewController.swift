af5b227546cc18f25d4cc0ae45138e5cd4345080
Naciye Çelenli <naciye.celenli@ecodation.com>
2022-02-06 15:54:13 +0300

    Initial Commit

---
+++ MovieManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieManager
+//
+//  Created by Naciye Celenli on 6.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
