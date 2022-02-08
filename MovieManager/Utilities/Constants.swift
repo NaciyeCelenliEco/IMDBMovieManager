f79d7901ee8f78801ce301e3da32513082e17ed6
Naciye Çelenli <naciye.celenli@ecodation.com>
2022-02-09 02:57:11 +0300

    Added service

---
+++ MovieManager/Utilities/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  MovieManager
+//
+//  Created by Naciye Celenli on 8.02.2022.
+//
+class Constants {
+    
+    struct Date {
+        static let serviceFormat                        = "yyyy-MM-dd"
+        static let outFormat                            = "dd/MM/yyyy"
+    }
+    struct Api {
+        static let Key                                   = "487cd85231c3062de018b6566f30963d"
+    }
+}
---
+++ MovieManager/Utilities/UserManager.swift
@@ -0,0 +1,11 @@
+//
+//  UserManager.swift
+//  MovieManager
+//
+//  Created by Naciye Celenli on 8.02.2022.
+//
+
+class UserManager {
+    static var accountId: Int = 0
+    static var sessionId: String = ""
+}
